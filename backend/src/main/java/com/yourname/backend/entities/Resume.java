6deae5a31deb18eba539cdff61909f7bf36e1eea
Siddarth Luthra <siddarthluthra@MacBook-Pro-5.local>
2025-04-16 12:55:16 -0400

    Implemented Resume.java and ResumeRepository.java

---
+++ backend/src/main/java/com/yourname/backend/entities/Resume.java
@@ -0,0 +1,61 @@
+package com.yourname.backend.entities;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "resumes")
+public class Resume {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+    private String candidateName;
+    private LocalDateTime uploadDate;
+
+    private String filePath;
+
+    public Resume() {
+    }
+
+    public Resume(String fileName, String candidateName, String filePath) {
+        this.fileName = fileName;
+        this.candidateName = candidateName;
+        this.filePath = filePath;
+        this.uploadDate = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public String getCandidateName() {
+        return candidateName;
+    }
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+    public LocalDateTime getUploadDate() {
+        return uploadDate;
+    }
+    public void setUploadDate(LocalDateTime uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+    public String getFilePath() {
+        return filePath;
+    }
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
---
+++ backend/src/main/java/com/yourname/backend/repositories/ResumeRepository.java
@@ -0,0 +1,9 @@
+package com.yourname.backend.repositories;
+
+import com.yourname.backend.entities.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+}
